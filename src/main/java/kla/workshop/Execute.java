9cfb0aa3894d3083518759ec81941abbb85f8eef
Saurabh-Sharma <saurabh.ss214@gmail.com>
2022-03-08 13:44:56 +0530

    Comments Added

--- src/main/java/kla/workshop/Execute.java
+++ src/main/java/kla/workshop/Execute.java
@@ -8,22 +8,35 @@ public class Execute {
 	
 	public void execute(Map<String, Object> workflow) {
 		Map.Entry<String, Object> workflowMap = (Map.Entry<String, Object>)workflow.entrySet().iterator().next();
+		
+		// Setting the log message
 		String logStr = workflowMap.getKey();
+		
+		// Adding log to the log_file
 		System.out.println(getTime()+";"+logStr+" Entry");
+		
+		// Getting flow information
 		Map<String, Object> map = (Map<String, Object>)workflowMap.getValue();
+		
 		if(map.get("Type").toString().equals("Flow")) {
 			if(map.get("Execution").toString().equals("Sequential")) {
 				executeSequentialFlow((Map<String, Object>)map.get("Activities"), logStr);
 			}
 		}
-		System.out.println(getTime()+" "+logStr+" Exit");
+		System.out.print(getTime()+" "+logStr+" Exit");
 	}
 	
+	// Sequential Flow Execution
 	public void executeSequentialFlow(Map<String, Object> workflowMap, String logStr) {
 		for(Map.Entry<String, Object> valueObj: workflowMap.entrySet()) {
+			// Creating the log for flow
 			String currLogStr = valueObj.getKey();
+			
+			// Getting the flow information
 			Map<String, Object> map = (Map<String, Object>)valueObj.getValue();
 			String type = map.get("Type").toString(); 
+			
+			// Checking the Type for Task or Flow
 			if(type.equals("Task")) {
 				String function = (String)map.get("Function");
 				if(function.equals("TimeFunction")) {
@@ -31,18 +44,27 @@ public class Execute {
 					String functionInput = inputs.get("FunctionInput");
 					String executionTimeString = inputs.get("ExecutionTime");
 					int executionTime = Integer.parseInt(executionTimeString);
+					
+					// Printing the entry log
 					System.out.println(getTime()+" "+logStr+"."+currLogStr+" Entry");
+					
+					// Calling timeFunction with specified execution time
 					try {
 						timeFunction(functionInput, executionTime, logStr+"."+currLogStr);
 					}catch(Exception exc) {
 		
 					}
+					
+					// Printing the exit log
 					System.out.println(getTime()+" "+logStr+"."+currLogStr+" Exit");
 				}
 			}
 			else if(type.equals("Flow")) {
 				String execution = (String)map.get("Execution");
+				
+				// Checking the inner flow info
 				if(execution.equals("Sequential")) {
+					// If flow is sequential then recursively calling flow
 					System.out.println(getTime()+" "+logStr+"."+currLogStr+" Entry");
 					executeSequentialFlow((Map<String, Object>)map.get("Activities"), logStr+"."+currLogStr);
 					System.out.println(getTime()+" "+logStr+"."+currLogStr+" Exit");
@@ -52,9 +74,11 @@ public class Execute {
 	}
 
 	private void timeFunction(String functionInput, int executionTime, String logStr) throws InterruptedException {
-		System.out.println(getTime()+" "+logStr+" Executing TimeFunction");
-		Thread.sleep(executionTime*1000);
+		System.out.print(getTime()+" "+logStr+" Executing TimeFunction ");
 		System.out.println("("+functionInput+", "+executionTime+")");
+		
+		// Making Main thread sleep for executionTime seconds
+		Thread.sleep(executionTime*1000);
 	}
 	
 	public String getTime() {
